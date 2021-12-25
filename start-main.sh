487d179e553241d802be3b896395fb520ccd41b5
Ivan B <sensenabi@gmail.com>
2021-12-25 13:33:13 -0500

    automate hamachi

--- start-main.sh
+++ start-main.sh
@@ -4,8 +4,13 @@ NET_NAME=$1
 NET_PASS=$2
 
 service logmein-hamachi --full-restart
+sleep 5
+
 hamachi login
+sleep 5
+
 hamachi create $NET_NAME $NET_PASS
+sleep 5
 
 ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
 /usr/sbin/sshd -D
--- start-worker.sh
+++ start-worker.sh
@@ -4,8 +4,13 @@ NET_NAME=$1
 NET_PASS=$2
 
 service logmein-hamachi --full-restart
+sleep 5
+
 hamachi login
+sleep 5
+
 echo $NET_PASS | hamachi do-join $NET_NAME
+sleep 5
 
 ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
 /usr/sbin/sshd -D
